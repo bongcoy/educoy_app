d6e671b79105766e5c72e5d6bf20b5c309d623c0
bongcoy <mpascaldewantara@gmail.com>
2024-02-29 05:54:53 +0700

    feat: :hammer: update page and add a multidex support

--- lib/core/common/views/under_construction_page.dart
+++ lib/core/common/views/under_construction_page.dart
@@ -1,10 +1,28 @@
+import 'package:educoy_app/core/res/images.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class UnderConstructionPage extends StatelessWidget {
   const UnderConstructionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(MediaResource.onBoardingBackground),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Lottie.asset(MediaResource.underConstructionPage),
+          ),
+        ),
+      ),
+    );
   }
 }
