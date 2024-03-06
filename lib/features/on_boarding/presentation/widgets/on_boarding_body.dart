04817043753cfca48c6ba86a392af03a579821e8
bongcoy <mpascaldewantara@gmail.com>
2024-03-06 20:00:52 +0700

    feat: :hammer: add on_boarding_body.dart

---
+++ lib/features/on_boarding/presentation/widgets/on_boarding_body.dart
@@ -0,0 +1,13 @@
+import 'package:educoy_app/features/on_boarding/domain/entities/page_content.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingBody extends StatelessWidget {
+  const OnBoardingBody({required this.pageContent, super.key});
+
+  final PageContent pageContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
