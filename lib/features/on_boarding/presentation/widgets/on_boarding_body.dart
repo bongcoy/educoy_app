5b202d08a808c7a7177ad7a87fc392bbfe6f6b95
bongcoy <mpascaldewantara@gmail.com>
2024-03-06 20:36:11 +0700

    feat: :hammer: complete on_boarding_body.dart

--- lib/features/on_boarding/presentation/widgets/on_boarding_body.dart
+++ lib/features/on_boarding/presentation/widgets/on_boarding_body.dart
@@ -1,6 +1,10 @@
 import 'package:educoy_app/core/extensions/context_extension.dart';
+import 'package:educoy_app/core/res/colours.dart';
+import 'package:educoy_app/core/res/fonts.dart';
 import 'package:educoy_app/features/on_boarding/domain/entities/page_content.dart';
+import 'package:educoy_app/features/on_boarding/presentation/manager/on_boarding_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OnBoardingBody extends StatelessWidget {
   const OnBoardingBody({required this.pageContent, super.key});
@@ -14,6 +18,50 @@ class OnBoardingBody extends StatelessWidget {
         Image.asset(
           pageContent.image,
           height: context.height * .4,
+        ),
+        SizedBox(height: context.height * .04),
+        Padding(
+          padding: const EdgeInsets.all(20).copyWith(bottom: 0),
+          child: Column(
+            children: [
+              Text(
+                pageContent.title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontFamily: Fonts.aeonik,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: context.height * .02),
+              Text(
+                pageContent.description,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 14),
+              ),
+              SizedBox(height: context.height * .04),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 48,
+                    vertical: 16,
+                  ),
+                  backgroundColor: Colours.primaryColour,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  context.read<OnBoardingCubit>().cacheFirstTimer();
+                },
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontFamily: Fonts.aeonik,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
