03f199bf1bbea0fb22d1bc9b1e945ed162c5e604
bongcoy <mpascaldewantara@gmail.com>
2024-03-07 07:29:03 +0700

    test: :white_check_mark: add test domain layer -> auth use case

---
+++ test/features/auth/domain/usecases/sign_in_test.dart
@@ -0,0 +1,52 @@
+import 'package:dartz/dartz.dart';
+import 'package:educoy_app/features/auth/domain/entities/local_user.dart';
+import 'package:educoy_app/features/auth/domain/use_cases/sign_in.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'auth_repo.mock.dart';
+
+void main() {
+  late MockAuthRepo repo;
+  late SignIn usecase;
+
+  const tEmail = 'Test email';
+  const tPassword = 'Test password';
+
+  setUp(() {
+    repo = MockAuthRepo();
+    usecase = SignIn(repo);
+  });
+
+  final testUser = LocalUser.empty();
+
+  test(
+    'should return [LocalUser] from the [AuthRepo]',
+    () async {
+      when(
+        () => repo.signIn(
+          email: any(named: 'email'),
+          password: any(named: 'password'),
+        ),
+      ).thenAnswer((_) async => Right(testUser));
+
+      final result = await usecase(
+        const SignInParams(
+          email: tEmail,
+          password: tPassword,
+        ),
+      );
+
+      expect(result, Right<dynamic, LocalUser>(testUser));
+
+      verify(
+        () => repo.signIn(
+          email: tEmail,
+          password: tPassword,
+        ),
+      ).called(1);
+
+      verifyNoMoreInteractions(repo);
+    },
+  );
+}
