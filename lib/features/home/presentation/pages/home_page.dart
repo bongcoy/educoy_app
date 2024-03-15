9592b97e0e7fb7e26ff4627ec431b2bb0cab8d40
bongcoy <mpascaldewantara@gmail.com>
2024-03-15 22:53:52 +0700

    feat: :construction: add home page

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,26 +1,107 @@
+import 'package:educoy_app/core/res/colours.dart';
+import 'package:educoy_app/features/auth/data/models/local_user_model.dart';
+import 'package:educoy_app/features/home/presentation/manager/home_controller.dart';
+import 'package:educoy_app/features/home/presentation/manager/home_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:iconly/iconly.dart';
+import 'package:provider/provider.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-  final String title;
+  static const routeName = '/dashboard';
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text(''),
-      ),
+    return StreamBuilder<LocalUserModel>(
+      stream: HomeUtils.userDataStream,
+      builder: (_, snapshot) {
+        if (snapshot.hasData && snapshot.data is LocalUserModel) {
+          context.read<UserProvider>().user = snapshot.data;
+        }
+        return Consumer<DashboardController>(
+          builder: (_, controller, __) {
+            return Scaffold(
+              body: IndexedStack(
+                index: controller.currentIndex,
+                children: controller.screens,
+              ),
+              bottomNavigationBar: BottomNavigationBar(
+                currentIndex: controller.currentIndex,
+                showSelectedLabels: false,
+                backgroundColor: Colors.white,
+                elevation: 8,
+                onTap: controller.changeIndex,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      controller.currentIndex == 0
+                          ? IconlyBold.home
+                          : IconlyLight.home,
+                      color: controller.currentIndex == 0
+                          ? Colours.primaryColour
+                          : Colors.grey,
+                    ),
+                    label: 'Home',
+                    backgroundColor: Colors.white,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      controller.currentIndex == 1
+                          ? IconlyBold.document
+                          : IconlyLight.document,
+                      color: controller.currentIndex == 1
+                          ? Colours.primaryColour
+                          : Colors.grey,
+                    ),
+                    label: 'Materials',
+                    backgroundColor: Colors.white,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      controller.currentIndex == 2
+                          ? IconlyBold.chat
+                          : IconlyLight.chat,
+                      color: controller.currentIndex == 2
+                          ? Colours.primaryColour
+                          : Colors.grey,
+                    ),
+                    label: 'Chat',
+                    backgroundColor: Colors.white,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      controller.currentIndex == 3
+                          ? IconlyBold.profile
+                          : IconlyLight.profile,
+                      color: controller.currentIndex == 3
+                          ? Colours.primaryColour
+                          : Colors.grey,
+                    ),
+                    label: 'User',
+                    backgroundColor: Colors.white,
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
