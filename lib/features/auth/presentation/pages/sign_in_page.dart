1ed375d3c26f42103a2f4e96e79726743f194f36
bongcoy <mpascaldewantara@gmail.com>
2024-03-15 22:53:15 +0700

    feat: :construction: add auth pages

---
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -0,0 +1,129 @@
+import 'package:educoy_app/core/common/widgets/gradient_background.dart';
+import 'package:educoy_app/core/res/fonts.dart';
+import 'package:educoy_app/core/res/images.dart';
+import 'package:educoy_app/features/auth/data/models/local_user_model.dart';
+import 'package:educoy_app/features/auth/presentation/manager/auth_bloc.dart';
+import 'package:educoy_app/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  static const routeName = '/sign-in';
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (_, state) {
+          if (state is AuthError) {
+            CoreUtils.showSnackBar(context, state.message);
+          } else if (state is SignedIn) {
+            context.read<UserProvider>().initUser(state.user as LocalUserModel);
+            Navigator.pushReplacementNamed(context, Dashboard.routeName);
+          }
+        },
+        builder: (context, state) {
+          return GradientBackground(
+            image: MediaResource.authGradientBackground,
+            child: SafeArea(
+              child: Center(
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  children: [
+                    const Text(
+                      'Easy to learn, discover more skills.',
+                      style: TextStyle(
+                        fontFamily: Fonts.aeonik,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 32,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Sign in to your account',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        Baseline(
+                          baseline: 100,
+                          baselineType: TextBaseline.alphabetic,
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(
+                                context,
+                                SignUpPage.routeName,
+                              );
+                            },
+                            child: const Text('Register account?'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    SignInForm(
+                      emailController: emailController,
+                      passwordController: passwordController,
+                      formKey: formKey,
+                    ),
+                    const SizedBox(height: 20),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/forgot-password');
+                        },
+                        child: const Text('Forgot password?'),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    if (state is AuthLoading)
+                      const Center(child: CircularProgressIndicator())
+                    else
+                      RoundedButton(
+                        label: 'Sign In',
+                        onPressed: () {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          FirebaseAuth.instance.currentUser?.reload();
+                          if (formKey.currentState!.validate()) {
+                            context.read<AuthBloc>().add(
+                              SignInEvent(
+                                email: emailController.text.trim(),
+                                password: passwordController.text.trim(),
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/presentation/pages/sign_up_page.dart
@@ -0,0 +1,128 @@
+import 'package:educoy_app/core/common/widgets/gradient_background.dart';
+import 'package:educoy_app/core/res/fonts.dart';
+import 'package:educoy_app/core/res/images.dart';
+import 'package:educoy_app/features/auth/data/models/local_user_model.dart';
+import 'package:educoy_app/features/auth/presentation/manager/auth_bloc.dart';
+import 'package:educoy_app/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  static const routeName = '/sign-up';
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final emailController = TextEditingController();
+  final fullNameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    fullNameController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (_, state) {
+          if (state is AuthError) {
+            CoreUtils.showSnackBar(context, state.message);
+          } else if (state is SignedUp) {
+            context.read<AuthBloc>().add(
+                  SignInEvent(
+                    email: emailController.text.trim(),
+                    password: passwordController.text.trim(),
+                  ),
+                );
+          } else if (state is SignedIn) {
+            context.read<UserProvider>().initUser(state.user as LocalUserModel);
+            Navigator.pushReplacementNamed(context, Dashboard.routeName);
+          }
+        },
+        builder: (context, state) {
+          return GradientBackground(
+            image: MediaResource.authGradientBackground,
+            child: SafeArea(
+              child: Center(
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  children: [
+                    const Text(
+                      'Easy to learn, discover more skills.',
+                      style: TextStyle(
+                        fontFamily: Fonts.aeonik,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 32,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Sign up for an account',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    const SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                            context,
+                            SignInPage.routeName,
+                          );
+                        },
+                        child: const Text('Already have an account?'),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SignUpForm(
+                      emailController: emailController,
+                      fullNameController: fullNameController,
+                      passwordController: passwordController,
+                      confirmPasswordController: confirmPasswordController,
+                      formKey: formKey,
+                    ),
+                    const SizedBox(height: 30),
+                    if (state is AuthLoading)
+                      const Center(child: CircularProgressIndicator())
+                    else
+                      RoundedButton(
+                        label: 'Sign Up',
+                        onPressed: () {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          FirebaseAuth.instance.currentUser?.reload();
+                          if (formKey.currentState!.validate()) {
+                            context.read<AuthBloc>().add(
+                                  SignUpEvent(
+                                    email: emailController.text.trim(),
+                                    password: passwordController.text.trim(),
+                                    name: fullNameController.text.trim(),
+                                  ),
+                                );
+                          }
+                        },
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
