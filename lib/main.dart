b5576428dec73124f364c413d0735e047ba7bfcf
bongcoy <mpascaldewantara@gmail.com>
2024-03-17 20:42:25 +0700

    chore: :building_construction: add provider

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ import 'package:educoy_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'core/common/manager/user_provider.dart';
+import 'features/home/presentation/manager/home_controller.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,16 +29,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Educational App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
-        useMaterial3: true,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: Fonts.poppins,
-        appBarTheme: const AppBarTheme(color: Colors.transparent),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => HomeController()),
+      ],
+      child: MaterialApp(
+        title: 'Educational App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
+          useMaterial3: true,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: Fonts.poppins,
+          appBarTheme: const AppBarTheme(color: Colors.transparent),
+        ),
+        onGenerateRoute: generateRoute,
       ),
-      onGenerateRoute: generateRoute,
     );
   }
 }
