e3176bcb81a19a6f48c6febd0360828d17874b3e
bongcoy <mpascaldewantara@gmail.com>
2024-03-18 10:14:36 +0700

    feat: :hammer: add edit profile form field

---
+++ lib/features/profile/presentation/widgets/edit_profile_form_field.dart
@@ -0,0 +1,40 @@
+import 'package:educoy_app/core/common/widgets/text_form_field_educoy.dart';
+import 'package:flutter/material.dart';
+
+class EditProfileFormField extends StatelessWidget {
+  const EditProfileFormField({
+    required this.fieldTitle,
+    required this.controller,
+    super.key,
+    this.hintText,
+    this.readOnly = false,
+  });
+
+  final String fieldTitle;
+  final TextEditingController controller;
+  final String? hintText;
+  final bool readOnly;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(
+            fieldTitle,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+          ),
+        ),
+        const SizedBox(height: 10),
+        TextFormFieldEducoy(
+          controller: controller,
+          hintText: hintText,
+          readOnly: readOnly,
+        ),
+        const SizedBox(height: 30),
+      ],
+    );
+  }
+}
