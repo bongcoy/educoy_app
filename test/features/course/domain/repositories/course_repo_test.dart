f927b3296a1306f2a00885e120514bf75dec4f35
bongcoy <mpascaldewantara@gmail.com>
2024-03-18 13:18:59 +0700

    feat: :white_check_mark: add course domain layer -> repository

---
+++ test/features/course/domain/repositories/course_repo_test.dart
@@ -0,0 +1,4 @@
+import 'package:educoy_app/features/course/domain/repositories/course_repo.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockCourseRepo extends Mock implements CourseRepo {}
